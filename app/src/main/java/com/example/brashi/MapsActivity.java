b9c609c96da36fb7773305b463d5f7166a76c7fe
Slawek <slawek.lewandowski@as4you.pl>
2019-05-20 14:36:04 +0200

    masakra z tym gitem

--- app/src/main/java/com/example/brashi/MapsActivity.java
+++ app/src/main/java/com/example/brashi/MapsActivity.java
@@ -13,7 +13,6 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private GoogleMap dupa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
