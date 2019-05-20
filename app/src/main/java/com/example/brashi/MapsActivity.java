61337a4430e94525a06f52155d4e1449cd7c9998
Slawek <slawek.lewandowski@as4you.pl>
2019-05-20 15:52:48 +0200

    masakra z tym gitem

--- app/src/main/java/com/example/brashi/MapsActivity.java
+++ app/src/main/java/com/example/brashi/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.example.brashi;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +16,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    public Button listaOdnosnik;
+
+    public void init(){
+        listaOdnosnik = (Button)findViewById(R.id.buttonLista);
+        listaOdnosnik.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent przenies = new Intent(MapsActivity.this, Lista.class);
+                startActivity(przenies);
+            }
+        });
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +39,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        init();
     }
 
 
