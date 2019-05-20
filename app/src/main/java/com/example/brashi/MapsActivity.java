89474c4e672104d4c59f1ce40e62f715f8ffc90e
Slawek <slawek.lewandowski@as4you.pl>
2019-05-20 12:58:10 +0200

    pierwszy commit

--- app/src/main/java/com/example/brashi/MapsActivity.java
+++ app/src/main/java/com/example/brashi/MapsActivity.java
@@ -21,8 +21,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+       // mapFragment.getMapAsync(this);
     }
+
+
 
 
     /**
